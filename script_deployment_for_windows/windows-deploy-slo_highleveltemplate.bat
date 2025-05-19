d507e84dd6c639b0e0f905d73f4ed6a3e81e31f3
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2025-05-19 11:36:40 +0200

    Add files via upload

---
+++ script_deployment_for_windows/windows-deploy-slo_highleveltemplate.bat
@@ -0,0 +1,54 @@
+#slo_period_day default 7
+set Slo_period_day=21
+
+#Target
+set Target_highslo=98
+set Target_highslo_userexperiencevitallcp=%Target_highslo%
+set Target_highslo_userexperiencesuccessrate=%Target_highslo%
+set Target_highslo_servicesuccessrate=%Target_highslo%
+set Target_highslo_servicemethodsuccessrate=%Target_highslo%
+
+#Warning
+set Warning_highslo=99
+set Warning_highslo_userexperiencevitallcp=%Warning_highslo%
+set Warning_highslo_userexperiencesuccessrate=%Warning_highslo%
+set Warning_highslo_servicesuccessrate=%Warning_highslo%
+set Warning_highslo_servicemethodsuccessrate=%Warning_highslo%
+
+#Burnrate
+set Burnrate_highslo=4
+set Burnrate_highslo_userexperiencevitallcp=%Burnrate_highslo%
+set Burnrate_highslo_userexperiencesuccessrate=%Burnrate_highslo%
+set Burnrate_highslo_servicesuccessrate=%Burnrate_highslo%
+set Burnrate_highslo_servicemethodsuccessrate=%Burnrate_highslo%
+
+#Violatingsamples
+set Violatingsamples_highslo_userexperience=10
+set Violatingsamples_highslo=2
+set Violatingsamples_highslo_userexperiencevitallcp=%Violatingsamples_highslo_userexperience%
+set Violatingsamples_highslo_userexperiencesuccessrate=%Violatingsamples_highslo_userexperience%
+set Violatingsamples_highslo_servicesuccessrate=%Violatingsamples_highslo%
+set Violatingsamples_highslo_servicemethodsuccessrate=%Violatingsamples_highslo%
+
+#Slingwindow
+set Slingwindow_highslo_userexperience=15
+set Slingwindow_highslo=5
+set Slingwindow_highslo_userexperiencevitallcp=%Slingwindow_highslo_userexperience%
+set Slingwindow_highslo_userexperiencesuccessrate=%Slingwindow_highslo_userexperience%
+set Slingwindow_highslo_servicesuccessrate=%Slingwindow_highslo%
+set Slingwindow_highslo_servicemethodsuccessrate=%Slingwindow_highslo%
+
+#Dealertingsamples
+set Dealertingsamples_highslo=5
+set Dealertingsamples_highslo_userexperiencevitallcp=%Dealertingsamples_highslo%
+set Dealertingsamples_highslo_userexperiencesuccessrate=%Dealertingsamples_highslo%
+set Dealertingsamples_highslo_servicesuccessrate=%Dealertingsamples_highslo%
+set Dealertingsamples_highslo_servicemethodsuccessrate=%Dealertingsamples_highslo%
+
+#Rum on Grail
+set RumOnGrailDisabled=false
+
+monaco deploy manifest.yaml -e Env1 -p slo_highleveltemplate
+
+pause
+	
---
+++ script_deployment_for_windows/windows-deploy-slo_lowleveltemplate.bat
@@ -0,0 +1,54 @@
+#Rum on Grail
+set RumOnGrailDisabled=false
+
+#slo_period_day default 7
+set Slo_period_day=7
+
+#Target
+set Target_lowslo=90
+set Target_lowslo_userexperiencevitallcp=%Target_lowslo%
+set Target_lowslo_userexperiencesuccessrate=%Target_lowslo%
+set Target_lowslo_servicesuccessrate=%Target_lowslo%
+set Target_lowslo_servicemethodsuccessrate=%Target_lowslo%
+
+#Warning
+set Warning_lowslo=95
+set Warning_lowslo_userexperiencevitallcp=%Warning_lowslo%
+set Warning_lowslo_userexperiencesuccessrate=%Warning_lowslo%
+set Warning_lowslo_servicesuccessrate=%Warning_lowslo%
+set Warning_lowslo_servicemethodsuccessrate=%Warning_lowslo%
+
+#Burnrate
+set Burnrate_lowslo=20
+set Burnrate_lowslo_userexperiencevitallcp=%Burnrate_lowslo%
+set Burnrate_lowslo_userexperiencesuccessrate=%Burnrate_lowslo%
+set Burnrate_lowslo_servicesuccessrate=%Burnrate_lowslo%
+set Burnrate_lowslo_servicemethodsuccessrate=%Burnrate_lowslo%
+
+#Violatingsamples
+set Violatingsamples_lowslo_userexperience=20
+set Violatingsamples_lowslo=5
+set Violatingsamples_lowslo_userexperiencevitallcp=%Violatingsamples_lowslo_userexperience%
+set Violatingsamples_lowslo_userexperiencesuccessrate=%Violatingsamples_lowslo_userexperience%
+set Violatingsamples_lowslo_servicesuccessrate=%Violatingsamples_lowslo%
+set Violatingsamples_lowslo_servicemethodsuccessrate=%Violatingsamples_lowslo%
+
+#Slingwindow
+set Slingwindow_lowslo_userexperience=30
+set Slingwindow_lowslo=10
+set Slingwindow_lowslo_userexperiencevitallcp=%Slingwindow_lowslo_userexperience%
+set Slingwindow_lowslo_userexperiencesuccessrate=%Slingwindow_lowslo_userexperience%
+set Slingwindow_lowslo_servicesuccessrate=%Slingwindow_lowslo%
+set Slingwindow_lowslo_servicemethodsuccessrate=%Slingwindow_lowslo%
+
+#Dealertingsamples
+set Dealertingsamples_lowslo=5
+set Dealertingsamples_lowslo_userexperiencevitallcp=%Dealertingsamples_lowslo%
+set Dealertingsamples_lowslo_userexperiencesuccessrate=%Dealertingsamples_lowslo%
+set Dealertingsamples_lowslo_servicesuccessrate=%Dealertingsamples_lowslo%
+set Dealertingsamples_lowslo_servicemethodsuccessrate=%Dealertingsamples_lowslo%
+
+monaco deploy manifest.yaml -e Env1 -p slo_lowleveltemplate
+
+pause
+	
